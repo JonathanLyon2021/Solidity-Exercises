4787ad184e5d2f56c7a56d0711753a85ab21b84a
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-07-18 00:36:05 -0400

    Update DataLocation.sol
    Implemented a user defined struct. Person on line 9.

--- DataLocation.sol
+++ DataLocation.sol
@@ -4,4 +4,13 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract StorageLocation {
     Person[] public persons; //Is Person[]  the persons variable?
+   
+   //User defined struct
+    struct Person {
+        string name;
+        uint age;
+        address personAddress;
     }
+   
+   
+   }
