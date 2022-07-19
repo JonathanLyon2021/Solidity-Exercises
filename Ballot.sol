85f3cf08b6484f7cc9d0116011b9ae334b68e25f
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-07-19 14:16:05 -0400

    Create Ballot.sol
    Created contract Ballot and instantiated struct voter.

---
+++ Ballot.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/** 
+ * @title Ballot
+ * @dev Implements voting process along with vote delegation
+ */
+contract Ballot {
+
+    struct Voter {
+        uint weight; // weight is accumulated by delegation
+        bool voted;  // if true, that person already voted
+        address delegate; // person delegated to
+        uint vote;   // index of the voted proposal
+    }
+    
+}
