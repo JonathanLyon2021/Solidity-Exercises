ef255324225ced260c49daaddb928bb3eca9ff90
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-07-19 14:26:11 -0400

    Update Ballot.sol
    Created function vote on line 100.

--- Ballot.sol
+++ Ballot.sol
@@ -97,4 +97,18 @@ contract Ballot {
      * @dev Give your vote (including votes delegated to you) to proposal 'proposals[proposal].name'.
      * @param proposal index of proposal in the proposals array
      */
+       function vote(uint proposal) public {
+        Voter storage sender = voters[msg.sender];
+        require(sender.weight != 0, "Has no right to vote");
+        require(!sender.voted, "Already voted.");
+        sender.voted = true;
+        sender.vote = proposal;
+
+        // If 'proposal' is out of the range of the array,
+        // this will throw automatically and revert all
+        // changes.
+        proposals[proposal].voteCount += sender.weight;
+        
+        
+    }
 }
