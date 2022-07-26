4e646e5b2ae9c61493829df257bad37008c09b5b
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-07-26 00:16:29 -0400

    Update DelegateProxy.sol
    Added an external, payable function on line 8. Which initially reacts to receiving ethers.

--- DelegateProxy.sol
+++ DelegateProxy.sol
@@ -4,5 +4,9 @@ pragma solidity ^0.6.0;
 
 contract DelegateProxy {
     address internal implementation;
+     
+     function() external payable {
+        address addr = implementation;
     
+    }
 }
