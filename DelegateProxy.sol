4b95275f35b60070ba1497a179c9a451d8e0b843
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-07-26 00:20:58 -0400

    Update DelegateProxy.sol
    Added assembly on line 11.

--- DelegateProxy.sol
+++ DelegateProxy.sol
@@ -7,6 +7,15 @@ contract DelegateProxy {
      
      function() external payable {
         address addr = implementation;
+        
+        assembly {
+            calldatacopy(0, 0, calldatasize())
+            let result := delegatecall(gas(), addr, 0, calldatasize(), 0, 0)
+            returndatacopy(0, 0, returndatasize())
+            switch result
+            case 0 { revert(0, returndatasize()) }
+            default { return(0, returndatasize()) }
+        }
     
     }
 }
