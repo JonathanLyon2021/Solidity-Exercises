4f8e01bfb84a06eb01bd64d82e2f1eca15f19e94
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-07-31 16:57:14 -0400

    Update MyContract.sol
    Added a public getter function to MyContract, which is a view only function and returns a string(which is technically the variable "value").

--- MyContract.sol
+++ MyContract.sol
@@ -6,9 +6,14 @@ contract MyContract {
     string value;
     //this value represents a variable local to this contract. Stored in storage on the blockchain.
    
-   constructor() public {
+  constructor() public {
         value = "myValue";
     }
-
+  
+  function get() public view returns(string) {
+        return value;
+        //"public" sets the visiblity to this contract available to anyone on the blockchain.
+        //"value" is the same variable that is on line 6, which is always a string.
+    }
     
 }
