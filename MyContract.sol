8dc9bcdbf90d41811b820e106a871d0453bf66df
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-07-31 16:54:11 -0400

    Create MyContract.sol

---
+++ MyContract.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.4.24;
+
+contract MyContract {
+    string value;
+    //this value represents a variable local to this contract. Stored in storage on the blockchain.
+}
