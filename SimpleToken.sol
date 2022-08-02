eaf3a90ed64710344e86ac196ced2be6efd8a7eb
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-01 22:03:53 -0400

    Update SimpleToken.sol
    Added comments above contract.

--- SimpleToken.sol
+++ SimpleToken.sol
@@ -6,6 +6,13 @@ pragma solidity ^0.5.0;
 import "http://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "http://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
+/**
+ * @title SimpleToken
+ * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
+ * Note they can later distribute these tokens as they wish using `transfer` and other
+ * `ERC20` functions.
+ */
+
 contract SimpleToken is ERC20, ERC20Detailed {
 
     /**
