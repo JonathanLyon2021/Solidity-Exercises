93e4f7ce3e01bb9169d9806a9a612ddc1c28033c
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-01 22:02:44 -0400

    Create SimpleToken.sol

---
+++ SimpleToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.1;
+
+pragma solidity ^0.5.0;
+
+import "http://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "http://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+
+contract SimpleToken is ERC20, ERC20Detailed {
+
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+   
+}
+
+
+
+
