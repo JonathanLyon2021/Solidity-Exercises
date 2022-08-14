e1823b83052936f8122dc18d68cb9dd32fd4ce32
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-13 22:43:42 -0400

    Update Researcher.sol

--- Researcher.sol
+++ Researcher.sol
@@ -5,7 +5,10 @@ struct Researcher {
     string last_name;
     address researcher_address;
     
-// List of academic articles published by the Researcher
-string[] academic_papers;
+        // List of academic articles published by the Researcher
+        string[] academic_papers;
 
+    // List of researchers that can peer-review the Researcher's
+            // academic articles before publishing
+         Researcher[] peer_reviewers;
 }
