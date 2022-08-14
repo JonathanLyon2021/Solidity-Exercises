2fc23d76d04043a0b726f421f7aa7efbb32bbdc5
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-13 22:45:09 -0400

    Update Researcher.sol

--- Researcher.sol
+++ Researcher.sol
@@ -8,7 +8,12 @@ struct Researcher {
         // List of academic articles published by the Researcher
         string[] academic_papers;
 
-    // List of researchers that can peer-review the Researcher's
+         // List of researchers that can peer-review the Researcher's
             // academic articles before publishing
          Researcher[] peer_reviewers;
+         
+     // If an address is mapped to `true`, the Researcher can review
+    // the academic papers of the Researcher 
+    // associated with the address
+        mapping(address => bool) can_peer_review;
 }
