8b7cbeb921a00e98c66f6a6fd107a81ea657bd1c
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-13 22:35:52 -0400

    Update Structs.sol

--- Structs.sol
+++ Structs.sol
@@ -10,4 +10,17 @@ struct Account {
       
 }
 
+
+
+}
+
+
+// 1) The procedural way
+function method_1(address _address, uint _age, string _first_name, string _last_name) {
+    
+    Instructor memory instructor = instructors[_address];
+    instructor.age = _age;
+    instructor.first_name = _first_name;
+    instructor.last_name = _last_name;
+    instructorAccounts.push(_address) — 1;
 }
